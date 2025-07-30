27deafbc5e43dedb14c91b44967f62b01ba9d81a
Abdulaziz <neonblade770@gmail.com>
2025-07-31 02:06:00 +0500

    V

---
+++ app.js
@@ -0,0 +1,33 @@
+// 1
+
+// function isPalindrome(s) {
+//     const cleaned = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+//     return cleaned === cleaned.split('').reverse().join('');
+// }
+
+// const s = "A man, a plan, a canal: Panama";
+// console.log(isPalindrome(s));
+
+
+
+// 2
+
+// function isPalindrome(s) {
+//     const cleaned = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+//     return cleaned === cleaned.split('').reverse().join('');
+// }
+
+// const s = "race a car";
+// console.log(isPalindrome(s));
+
+
+
+// 3
+
+// function isPalindrome(s) {
+//     const cleaned = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+//     return cleaned === cleaned.split('').reverse().join('');
+// }
+
+// const s = " ";
+console.log(isPalindrome(s));
